f25b6fa233c63730b6580c9d7e238987569ea6de
Stina Persson <stpe1901@student.miun.se>
2025-04-15 09:59:36 +0200

    lagt in styling och fontstorlekar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,19 @@ function HomePage() {
   return (
     <div>
         <h1>Startsidan</h1>
+
+        <h2>H2 rubrik</h2>
+
+        <h3>H3 rubrik</h3>
+
+        <h4>H4 rubrik</h4>
+
+        <p>Brödtext Lorem ipsum dolor sit amet, 
+            consectetur adipisicing elit. Consequatur alias 
+            architecto quaerat officia beatae ducimus velit doloribus 
+            fugit error! Itaque sapiente non illum consequatur nostrum 
+            commodi consequuntur vel, iste corrupti.
+        </p>
     </div>
   )
 }
