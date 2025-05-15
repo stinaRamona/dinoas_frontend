c84e9117c1ef2ae807816cff28dd853390c361ee
Stina Persson <stpe1901@student.miun.se>
2025-05-15 14:48:18 +0200

    header och startsida r responsiva

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,18 @@ import Wave from "../assets/mörkgrönWave.svg";
 import "../css/Header.css"; 
 import { useAuth } from "../context/AuthContext";
 import headerImg from "../assets/header_img_smaller.jpg"; 
+import { useState } from "react";
 
 function Header() {
     const location = useLocation();
 
     const {user, logout} = useAuth(); 
+    const [menuOpen, setMenuOpen] = useState(false); 
+
+    //sätter värdet till motsatsen av vad värdet är innan (true/false)
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen); 
+    }
 
     return (
         <>
@@ -16,10 +23,15 @@ function Header() {
                 <h1>Din Oas</h1>
             </div>
 
+            {/*Knapp för hamburgermeny*/}
+            <button id="hamburgerBtn" onClick={toggleMenu}>
+                ☰
+            </button>
+
             {/*Om man är på adminsidan (alltså inloggad) Döls den vanliga navigationen och man kan logga ut istället */}
             {
                 !user ? 
-            <ul>
+            <ul className={`nav-menu ${menuOpen ? "open" : ""}`}>
                 <li><NavLink className="nav-link" to="/">Start</NavLink></li>
                 <li><NavLink className="nav-link" to="/om">Om oss</NavLink></li>
                 <li><NavLink className="nav-link" to="/nyheter">Nyheter</NavLink></li>
